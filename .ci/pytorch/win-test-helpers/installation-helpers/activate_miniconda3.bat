cc34da4e39591484e512c25bc46c748232aa1e33
Catherine Lee <csl@fb.com>
2024-06-06 16:55:46 -0700

    tc

--- .ci/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.bat
+++ .ci/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.bat
@@ -2,10 +2,10 @@
   echo #!/bin/bash
   echo export INSTALLER_DIR="$SCRIPT_HELPERS_DIR"/installation-helpers
   echo source "$INSTALLER_DIR"/activate_miniconda3.sh
-  echo echo "set > tmp.txt" ^> tmp.bat
+  echo echo "set > %INSTALLER_DIR%\tmp.txt" ^> $INSTALLER_DIR/tmp.bat
   echo "$INSTALLER_DIR/tmp.bat"
 ) > %INSTALLER_DIR%\tmp.sh
-
+cat %INSTALLER_DIR%\tmp.sh
 bash %INSTALLER_DIR%\tmp.sh
 if errorlevel 1 exit /b
 if not errorlevel 0 exit /b
@@ -13,6 +13,6 @@ if not errorlevel 0 exit /b
 rm %INSTALLER_DIR%\tmp.sh
 rm %INSTALLER_DIR%\tmp.bat
 
-FOR /F "tokens=*" %%i in ('type tmp.txt') do SET %%i
+FOR /F "tokens=*" %%i in ('type %INSTALLER_DIR%\tmp.txt') do SET %%i
 
 rm %INSTALLER_DIR%\tmp.txt
